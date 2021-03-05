33f4b74a64745acfc9a3401f7e823951ee85ca0d
Henry Nunez <henry.nunez@clabs.co>
2021-03-05 12:59:51 -0500

    creating header array

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,26 @@
-export default function Table({}){
+let arrayHeader = ["Quarter/Period", "BTC Delta", "CELO Delta", "ETH Delta", "DAI Delta", "Dollar Value"]
+
+export default function Table(){
+
+    const renderBody = () =>{
+        return (
+            <tr>
+                <td>henry</td>
+                <td>lala</td>
+                <td>chu chu</td>
+                <td>chuchuchuchuc</td>
+            </tr>
+        )
+    }
+
+
     return(
         <div>
-            
+            <table>
+                <tbody>
+                    {renderBody()}
+                </tbody>
+            </table>
         </div>
     )
 }
