3c43aef629a98e573864150f752581c19163baa4
Aaron <aaron@clabs.co>
2021-03-09 13:55:55 -0800

    add total value, allign numbers etc

--- src/components/Amount.tsx
+++ src/components/Amount.tsx
@@ -13,7 +13,6 @@ interface AmountProps {
 
 export default function Amount({iconSrc, label, units, gridArea, context, value, loading }: AmountProps) {
   const display = new Intl.NumberFormat('default').format(Math.round(units))
-  const displayValue = value && Math.round(value).toLocaleString()
   const id = `a-${dasherize(label)}`
   return (
     <div css={css(amountStyle, { gridArea })}>
@@ -21,12 +20,25 @@ export default function Amount({iconSrc, label, units, gridArea, context, value,
         {iconSrc && <img width={30} height={30} src={iconSrc} css={iconStyle} alt="" />}
         <p id={id} css={labelCss}><abbr css={abbrCSS} title={context}>{label}</abbr></p>
       </div>
-      <span aria-labelledby={id} css={css(numberStyle, loading && notShowing )}>{display}</span>
-      <span aria-label={`Value of ${label} in USD`} css={css(dollarValueStyle, loading && notShowing)}>
-        {loading ? " ": !!value && `$${displayValue}`}
-      </span>
+      <span aria-labelledby={id} css={css(!loading && value ? secondaryNumberStyle : numberStyle, loading && notShowing )}>{display}</span>
+      <DollarDisplay css={primaryNumberStyle} value={value} loading={loading} label={label}  />
     </div>
   )
+}
+
+interface DollarDisplayProps {
+  value: number,
+  loading: boolean,
+  label: string,
+  className?: string
+}
+
+export function DollarDisplay({value, loading, label, className}: DollarDisplayProps) {
+  const displayValue = value && Math.round(value).toLocaleString()
+
+  return  <span className={className} aria-label={`Value of ${label} in USD`} css={css(dollarValueStyle, loading && notShowing)}>
+  {loading ? " ": !!value && `$${displayValue}`}
+</span>
 }
 
 const abbrCSS = css({
@@ -40,7 +52,10 @@ const notShowing = css({
 })
 
 const labelCss = css({
-  textAlign: "left"
+  textAlign: "left",
+  alignItems: "flex-end",
+  marginBottom: 0,
+  lineHeight: 1
 })
 
 function dasherize(str: string) {
@@ -51,7 +66,11 @@ const labelAreaCss = css({
   display: "inline-flex",
   flexDirection: "row",
   marginBottom: 12,
-  marginTop: 12
+  marginTop: 12,
+  alignItems: "flex-end",
+  [BreakPoints.tablet] : {
+    flex: 1,
+  }
 })
 
 const numberStyle = css({
@@ -61,19 +80,23 @@ const numberStyle = css({
   transitionDuration: "500ms",
   opacity: 1,
   fontSize: 36,
-  [BreakPoints.tablet]: {
-    textAlign: "right",
+  [BreakPoints.tablet] : {
+    flex: 1,
     fontSize: 28,
     marginBottom: 8,
-  },
+  }
 })
 
-const dollarValueStyle = css(numberStyle, {
-  color: colors.gray,
-  marginTop: 16,
+const primaryNumberStyle = css(numberStyle,  {
   [BreakPoints.tablet]: {
-    textAlign: "right"
-  },
+    textAlign: "right",
+    flex: 1,
+  }
+})
+
+const secondaryNumberStyle = css(numberStyle, {
+  textAlign: "left",
+  paddingleft: "2em",
   [BreakPoints.phablet]: {
     display: "none"
   },
@@ -82,11 +105,18 @@ const dollarValueStyle = css(numberStyle, {
   }
 })
 
+const dollarValueStyle = css(numberStyle, {
+  color: colors.gray,
+  marginTop: 16,
+  marginBottom: 16,
+})
+
 const amountStyle = css({
   [BreakPoints.tablet]: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'flex-end',
+
   },
 })
 const iconStyle = css({ height: 30, width: 30, marginRight: 8 })
