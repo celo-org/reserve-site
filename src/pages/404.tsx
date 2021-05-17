4afc39922b30b8c5df806e5a6c177b4076ea0f52
Aaron <aaron@clabs.co>
2021-05-17 11:00:09 -0700

    hnnm

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,8 @@
-
-import { css } from '@emotion/react'
-import Footer from 'src/components/Footer'
-import Head from 'src/components/Head'
-import NavBar from 'src/components/Navbar'
-import { flexCol, mainStyle, rootStyle } from 'src/components/styles'
-
+import { css } from "@emotion/react"
+import Footer from "src/components/Footer"
+import Head from "src/components/Head"
+import NavBar from "src/components/Navbar"
+import { flexCol, mainStyle, rootStyle } from "src/components/styles"
 interface Props {
   year: string
 }
@@ -28,11 +26,11 @@ export default function Page(props: Props) {
 }
 
 const bodyStyle = css(mainStyle, {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  alignContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  alignContent: "center",
 })
 
 const imageStyle = css({
@@ -40,9 +38,9 @@ const imageStyle = css({
   padding: 24,
 })
 
-const titleStyle = css({ textAlign: 'center', marginTop: 30 })
+const titleStyle = css({ textAlign: "center", marginTop: 30 })
 
-const containerStyle = css(flexCol, { flex: 1, width: '100%', alignItems: 'center' })
+const containerStyle = css(flexCol, { flex: 1, width: "100%", alignItems: "center" })
 
 export async function getStaticProps() {
   return {
